52dfab9262fc41b8902e20dddf27d828b88f22dc
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-01 09:29:18 -0500

    instuctions for importing contacts

--- components/SolidityContractDeployments.jsx
+++ components/SolidityContractDeployments.jsx
@@ -5,8 +5,13 @@ import {
 const SolidityContractDeployments = ({ deployments }) => {
   return (
     <div>
-      <h2 className='font-bold py-3'>Deployments</h2>
-      <div className='flex'>
+      <h2 className='font-bold pt-3'>Deployments</h2>
+      <div className='text-xs pt-1'>
+        Run `nod import` or `nod import [ADDRESS]` from any project to import contract information (networ, abi, and address).
+        <br/>
+        Example: `nod import {deployments[0].contractAddress}`
+      </div>
+      <div className='flex pt-3'>
         {deployments.sort(
           (a, b) => b.deployedAt - a.deployedAt
         ).map(
